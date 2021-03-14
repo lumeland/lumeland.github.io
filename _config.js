0d0181fddc29d3a123c5d4b147b3ff88a37f3216
Oscar Otero <oom@oscarotero.com>
2021-03-14 14:21:30 +0100

    load pages dynamically

--- _config.js
+++ _config.js
@@ -1,8 +1,9 @@
-import lume from "https://deno.land/x/lume@v0.15.0/mod.js";
+import lume from "https://deno.land/x/lume@v0.16.1/mod.js";
 
 const site = lume();
 
 site.ignore("README.md");
+site.copy("js");
 site.copy("styles.css");
 site.copy("logo.svg");
 site.copy("favicon.ico");
---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+import Navigator from "./page-loader/navigator.js";
+const menu = document.querySelector(".menu-links");
+
+const nav = new Navigator(async (load, event) => {
+  const page = await load();
+
+  await page.replaceContent("main");
+  await page.updateState();
+  await page.resetScroll();
+
+  menu.querySelectorAll('a[aria-current="page"]').forEach((link) =>
+    link.removeAttribute("aria-current")
+  );
+
+  event.target.setAttribute("aria-current", "page");
+});
+
+nav.init();
